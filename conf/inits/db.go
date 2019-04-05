76a11b11c844d6d2876d87bbeba0a0c005fa115f
Huy Duong <huy.duong@smec.com>
2019-04-05 15:32:03 +1100

    change to mysql

--- conf/inits/db.go
+++ conf/inits/db.go
@@ -8,7 +8,7 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
@@ -16,8 +16,8 @@ func init() {
 	datasource := beego.AppConfig.String("datasource") // sqlite file
 
 	orm.DefaultTimeLoc = time.Local
-	orm.RegisterDriver("sqlite3", orm.DRSqlite)
-	orm.RegisterDataBase(dbname, "sqlite3", datasource)
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase(dbname, "mysql", datasource)
 
 	// sync model and database
 	if err := orm.RunSyncdb(dbname, false, true); err != nil {
